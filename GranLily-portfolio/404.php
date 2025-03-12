0df5110897378c15e7c5fca87cf6854fb2b6eecd
lilychaco <lilychaco@gmail.com>
2025-03-12 20:21:55 +0900

    404ページのデザイン修正

--- GranLily-portfolio/404.php
+++ GranLily-portfolio/404.php
@@ -1,12 +1,7 @@
 <?php get_header(); ?>
 
-<div class="page-404">
+<!-- <div class="page-404">
 	<div class="page-404__inner inner">
-		<div class="breadcrumb-wrapper">
-			<div class="page-404__breadcrumb breadcrumb">
-				<a href="<?php echo esc_url(home_url('/')); ?>">TOP</a>&nbsp;&nbsp;>&nbsp;&nbsp;<a href="#">404</a>
-			</div>
-		</div>
 		<div class="page-404__content">
 			<p class="pave-404__title">404</p>
 			<p class="page-404__text">申し訳ありません。<br>
@@ -16,6 +11,29 @@
 			</div>
 		</div>
 	</div>
+</div> -->
+<style>
+.container {
+	max-width: 600px;
+	margin: 100px auto;
+}
+</style>
+<div class="page-404">
+	<div class="page-404__inner inner">
+		<div class="page-404__container">
+			<h2 class="page-404__title">404</h2>
+
+			<div class="page-404__img">
+				<img src="<?php echo esc_url(get_theme_file_uri('assets/images/logo-granlily-long3.png')); ?>"
+					alt="Granlily Character">
+			</div>
+
+			<p class="page-404__text">申し訳ありません。お探しのページが見つかりません。</p>
+			<p class="page-404__text">The page you're looking for might have been moved, deleted, or never existed.</p>
+			<a href="/" class="page-404__button button--02">トップページへ</a>
+		</div>
+	</div>
 </div>
+
 
 <?php get_footer(); ?>
